5cdd9c99e854992a4313d897fcda1f7a7d404fe6
Julius Tens <mail@juliustens.eu>
2020-10-11 16:37:13 +0200

    init: first draft

---
+++ build.js
@@ -0,0 +1,68 @@
+'use strict'
+
+// the data is fetched and processed in these steps:
+// 1. obtain a session cookie and the zip file's url
+// 2. download the data using the session cookie
+
+const got = require('got')
+const cheerio = require('cheerio')
+
+const extractUrlFromResponse = (html, isMatchingFile) => {
+	const parsed = cheerio.load(html)
+	const url = parsed('a').filter(function (i, el) {
+		// this === el
+		return isMatchingFile(parsed(this).attr('href') || '')
+	}).attr('href')
+	if (!url) throw new Error('no matching url found on download page. internal error or invalid credentials')
+	return url
+}
+
+const fetchAndOutput = async (user, password, datasetName, isMatchingFile) => {
+	const url = new URL('https://www.opendata-oepnv.de/ht/de/organisation/delfi/startseite')
+	url.searchParams.append('tx_vrrkit_view[dataset_name]', datasetName)
+	url.searchParams.append('tx_vrrkit_view[action]', 'details')
+	url.searchParams.append('tx_vrrkit_view[controller]', 'View')
+
+	const response = await got.post(url, {
+		form: {
+			user,
+			pass: password,
+			submit: 'Anmelden',
+			logintype: 'login',
+			pid: 174,
+			'tx_felogin_pi1[noredirect]': 0,
+			referer: url.toString(),
+		},
+	})
+
+	const cookie = (response.headers['set-cookie'] || []).find(c => c.includes('fe_typo_user'))
+	if (!cookie) throw new Error('cookie not found. internal error or invalid credentials')
+
+	const fileUrl = extractUrlFromResponse(response.body, isMatchingFile)
+
+	const stream = await got.stream.get(fileUrl, { headers: { Cookie: cookie } })
+	return stream.pipe(process.stdout)
+}
+
+const logAndExit = error => {
+	console.error(error)
+	process.exit(1)
+}
+
+const [user, password] = [process.env.OPENDATA_OEPNV_EMAIL, process.env.OPENDATA_OEPNV_PASSWORD]
+if (typeof user !== 'string' || user.length === 0) logAndExit('env.OPENDATA_OEPNV_EMAIL must be a non-empty string')
+if (typeof password !== 'string' || password.length === 0) logAndExit('env.OPENDATA_OEPNV_PASSWORD must be a non-empty string')
+
+const gtfs = () => {
+	const datasetName = 'deutschlandweite-sollfahrplandaten-gtfs'
+	const isMatchingFile = name => name.endsWith('_fahrplaene_gesamtdeutschland_gtfs.zip')
+	fetchAndOutput(user, password, datasetName, isMatchingFile).catch(logAndExit)
+}
+
+const zhv = () => {
+	const datasetName = 'deutschlandweite-haltestellendaten'
+	const isMatchingFile = name => name.endsWith('_zHV_gesamt.zip')
+	fetchAndOutput(user, password, datasetName, isMatchingFile).catch(logAndExit)
+}
+
+module.exports = { gtfs, zhv }
---
+++ readme.md
@@ -0,0 +1,16 @@
+# opendata-oepnv
+
+Scraper that re-publishes official german public transport datasets under stable URLs, since the [official data portal](https://www.opendata-oepnv.de) sadly doesn't provide any on its own, making it nearly impossible to integrate these datasets in any automated systems.
+
+## Datasets
+
+The datasets are provided as [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/deed.de) by [opendata-oepnv.de](https://www.opendata-oepnv.de).
+
+Dataset | Stable URL
+------- | ---
+[Germany-wide GTFS feed](https://www.opendata-oepnv.de/ht/de/organisation/delfi/startseite?tx_vrrkit_view%5Bdataset_name%5D=deutschlandweite-sollfahrplandaten-gtfs&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View) | [latest](https://opendata-oepnv.fra1.digitaloceanspaces.com/gtfs-germany.zip)
+[German public transport stop registry (ZHV)](https://www.opendata-oepnv.de/ht/de/organisation/delfi/startseite?tx_vrrkit_view%5Bdataset_name%5D=deutschlandweite-haltestellendaten&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View) | [latest](https://opendata-oepnv.fra1.digitaloceanspaces.com/zhv.zip)
+
+## Contributing
+
+If you found a bug or want to propose a feature, feel free to visit [the issues page](https://github.com/juliuste/opendata-oepnv/issues).
