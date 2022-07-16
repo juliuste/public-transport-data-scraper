14b987c2591481f39024c7c80a19782b36ccfd2b
Julius Tens <mail@juliustens.eu>
2022-07-17 00:02:41 +0200

    add scraper for israeli gtfs feed (:il:)

--- src/run.sh
+++ src/run.sh
@@ -7,4 +7,11 @@ if [ -z ${MINIMUM_SIZE_MB+x} ]; then echo "missing env.MINIMUM_SIZE_MB"; exit 1;
 DIRECTORY=$(dirname "$0")
 FILE_PATH="$DIRECTORY/../$FILE_NAME"
 MINIMUM_SIZE_BYTES=$((1024 * 1024 * $MINIMUM_SIZE_MB))
-node $DIRECTORY/fetch.js > $FILE_PATH && (if [ $(wc -c $FILE_PATH | awk '{print $1}') -lt $MINIMUM_SIZE_BYTES ]; then (echo 'Unexpected file size, seems to small.'; exit 1;) fi;)
+
+if [[ $DATASET == "IL_GTFS" ]]; then
+	curl --output $FILE_PATH 'ftp://gtfs.mot.gov.il/israel-public-transportation.zip'
+else
+	node $DIRECTORY/fetch.js > $FILE_PATH;
+fi
+
+if [ $(wc -c $FILE_PATH | awk '{print $1}') -lt $MINIMUM_SIZE_BYTES ]; then (echo 'Unexpected file size, seems to small.'; exit 1;) fi
