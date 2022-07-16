4499aabb4157e7b0748b5712b4a52a27d9ebaf10
Julius Tens <mail@juliustens.eu>
2022-07-16 15:34:41 +0200

    add scraper for north rhine-westfalia gtfs feed (:de:)

--- src/fetch.js
+++ src/fetch.js
@@ -1,29 +1,31 @@
 const dataset = process.env.DATASET
 
 const main = async () => {
-	if (dataset === 'DE_NETEX') {
+	if (['DE_NETEX', 'DE_GTFS', 'DE_ZHV', 'DE_NRW_GTFS'].includes(dataset)) {
 		const [user, password] = [process.env.OPENDATA_OEPNV_EMAIL, process.env.OPENDATA_OEPNV_PASSWORD]
 		if (typeof user !== 'string' || user.length === 0) throw new Error('env.OPENDATA_OEPNV_EMAIL must be a non-empty string')
 		if (typeof password !== 'string' || password.length === 0) throw new Error('env.OPENDATA_OEPNV_PASSWORD must be a non-empty string')
-		const { netex } = await import('./scrapers/opendata-oepnv.js')
-		await netex(user, password)
-		return
-	}
-	if (dataset === 'DE_GTFS') {
-		const [user, password] = [process.env.OPENDATA_OEPNV_EMAIL, process.env.OPENDATA_OEPNV_PASSWORD]
-		if (typeof user !== 'string' || user.length === 0) throw new Error('env.OPENDATA_OEPNV_EMAIL must be a non-empty string')
-		if (typeof password !== 'string' || password.length === 0) throw new Error('env.OPENDATA_OEPNV_PASSWORD must be a non-empty string')
-		const { gtfs } = await import('./scrapers/opendata-oepnv.js')
-		await gtfs(user, password)
-		return
-	}
-	if (dataset === 'DE_ZHV') {
-		const [user, password] = [process.env.OPENDATA_OEPNV_EMAIL, process.env.OPENDATA_OEPNV_PASSWORD]
-		if (typeof user !== 'string' || user.length === 0) throw new Error('env.OPENDATA_OEPNV_EMAIL must be a non-empty string')
-		if (typeof password !== 'string' || password.length === 0) throw new Error('env.OPENDATA_OEPNV_PASSWORD must be a non-empty string')
-		const { zhv } = await import('./scrapers/opendata-oepnv.js')
-		await zhv(user, password)
-		return
+
+		if (dataset === 'DE_NETEX') {
+			const { netex } = await import('./scrapers/opendata-oepnv.js')
+			await netex(user, password)
+			return
+		}
+		if (dataset === 'DE_GTFS') {
+			const { gtfs } = await import('./scrapers/opendata-oepnv.js')
+			await gtfs(user, password)
+			return
+		}
+		if (dataset === 'DE_ZHV') {
+			const { zhv } = await import('./scrapers/opendata-oepnv.js')
+			await zhv(user, password)
+			return
+		}
+		if (dataset === 'DE_NRW_GTFS') {
+			const { nrwGtfs } = await import('./scrapers/opendata-oepnv.js')
+			await nrwGtfs(user, password)
+			return
+		}
 	}
 	throw new Error(`unknown dataset: ${dataset}`)
 }
--- src/scrapers/opendata-oepnv.js
+++ src/scrapers/opendata-oepnv.js
@@ -1,24 +1,30 @@
-'use strict'
-
 // the data is fetched and processed in these steps:
 // 1. obtain a session cookie and the zip file's url
 // 2. download the data using the session cookie
 
-import got from 'got'
+import { DateTime } from 'luxon'
 import cheerio from 'cheerio'
+import got from 'got'
+import lodash from 'lodash'
+import { basename } from 'path'
 
-const extractUrlFromResponse = (html, isMatchingFile) => {
+const throwOnMultipleOrNoMatches = fileUrls => {
+	if (fileUrls.length !== 1) throw new Error(`unexpected number of file urls: ${fileUrls.length}, probably internal error or invalid credentials.`)
+	return fileUrls[0]
+}
+
+const extractUrlFromResponse = (html, isMatchingFile, selectBestMatch) => {
 	const parsed = cheerio.load(html)
-	const url = parsed('a').filter(function (i, el) {
+	const urls = Array.from(parsed('a').filter(function (i, el) {
 		// this === el
 		return isMatchingFile(parsed(this).attr('href') || '')
-	}).attr('href')
-	if (!url) throw new Error('no matching url found on download page. internal error or invalid credentials')
+	})).map(a => parsed(a).attr('href'))
+	const url = selectBestMatch(urls)
 	return url
 }
 
-const fetchAndOutput = async (user, password, datasetName, isMatchingFile) => {
-	const url = new URL('https://www.opendata-oepnv.de/ht/de/organisation/delfi/startseite')
+const fetchAndOutput = async (user, password, organisationPath, datasetName, isMatchingFile, selectBestMatch = throwOnMultipleOrNoMatches) => {
+	const url = new URL(organisationPath, 'https://www.opendata-oepnv.de/ht/de/organisation/')
 	url.searchParams.append('tx_vrrkit_view[dataset_name]', datasetName)
 	url.searchParams.append('tx_vrrkit_view[action]', 'details')
 	url.searchParams.append('tx_vrrkit_view[controller]', 'View')
@@ -38,26 +44,47 @@ const fetchAndOutput = async (user, password, datasetName, isMatchingFile) => {
 	const cookie = (response.headers['set-cookie'] || []).find(c => c.includes('fe_typo_user'))
 	if (!cookie) throw new Error('cookie not found. internal error or invalid credentials')
 
-	const fileUrl = extractUrlFromResponse(response.body, isMatchingFile)
+	const fileUrl = extractUrlFromResponse(response.body, isMatchingFile, selectBestMatch)
 
 	const stream = await got.stream.get(fileUrl, { headers: { Cookie: cookie } })
 	return stream.pipe(process.stdout)
 }
 
 export const gtfs = async (user, password) => {
+	const organisationPath = 'delfi/startseite'
 	const datasetName = 'deutschlandweite-sollfahrplandaten-gtfs'
 	const isMatchingFile = name => name.endsWith('_fahrplaene_gesamtdeutschland_gtfs.zip')
-	await fetchAndOutput(user, password, datasetName, isMatchingFile)
+	await fetchAndOutput(user, password, organisationPath, datasetName, isMatchingFile)
 }
 
 export const netex = async (user, password) => {
+	const organisationPath = 'delfi/startseite'
 	const datasetName = 'deutschlandweite-sollfahrplandaten'
 	const isMatchingFile = name => name.endsWith('_fahrplaene_gesamtdeutschland.zip')
-	await fetchAndOutput(user, password, datasetName, isMatchingFile)
+	await fetchAndOutput(user, password, organisationPath, datasetName, isMatchingFile)
 }
 
 export const zhv = async (user, password) => {
+	const organisationPath = 'delfi/startseite'
 	const datasetName = 'deutschlandweite-haltestellendaten'
 	const isMatchingFile = name => name.endsWith('_zHV_gesamt.zip')
-	await fetchAndOutput(user, password, datasetName, isMatchingFile)
+	await fetchAndOutput(user, password, organisationPath, datasetName, isMatchingFile)
+}
+
+export const nrwGtfs = async (user, password) => {
+	const organisationPath = 'bundeslaender/nrw/startseite'
+	const datasetName = 'soll-fahrplandaten-nrw'
+	const isMatchingFile = name => /\/\d{8}_gtfs_nrw\.zip$/.test(name)
+	const selectBestMatch = urls => {
+		const urlsWithDate = urls.map(url => {
+			const fileName = basename(new URL(url).pathname)
+			const date = DateTime.fromFormat(fileName.slice(0, 8), 'yyyyMMdd').toJSDate()
+			return { date, url }
+		})
+		const latest = lodash.last(lodash.sortBy(urlsWithDate, ({ date }) => +date))
+		// throw if latest file is older than 90 days
+		if (+new Date() - (+latest.date) > 90 * 24 * 60 * 60 * 1000) throw new Error(`latest dataset seems to be outdated: ${latest.date}`)
+		return latest.url
+	}
+	await fetchAndOutput(user, password, organisationPath, datasetName, isMatchingFile, selectBestMatch)
 }
