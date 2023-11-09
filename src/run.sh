cd5ece3b16c884e13d6c181dd2de7f2f0082e270
Julius Tens <mail@juliustens.eu>
2023-11-09 06:08:41 +0100

    remove old feed for israel, since it is available via http(s) by now

--- src/run.sh
+++ src/run.sh
@@ -8,9 +8,7 @@ DIRECTORY=$(dirname "$0")
 FILE_PATH="$DIRECTORY/../$FILE_NAME"
 MINIMUM_SIZE_BYTES=$((1024 * 1024 * $MINIMUM_SIZE_MB))
 
-if [[ $DATASET == "IL_GTFS" ]]; then
-	curl --output $FILE_PATH 'ftp://gtfs.mot.gov.il/israel-public-transportation.zip'
-elif [[ $DATASET == "SE_GTFS" ]]; then
+if [[ $DATASET == "SE_GTFS" ]]; then
 	curl --output $FILE_PATH -L "https://api.resrobot.se/gtfs/sweden.zip?key=$TRAFIKLAB_API_KEY"
 else
 	node $DIRECTORY/fetch.js > $FILE_PATH;
