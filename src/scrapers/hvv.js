2aee9467ad0d8c3140e2e0cce139ac415c041f8e
Julius Tens <mail@juliustens.eu>
2024-12-27 13:38:52 +0100

    fix hvv feed

--- src/scrapers/hvv.js
+++ src/scrapers/hvv.js
@@ -1,7 +1,7 @@
 import got from 'got'
 
 export const hvvGtfs = async () => {
-	const response = await got.get(new URL('https://suche.transparenz.hamburg.de/api/3/action/package_search?q=gtfs&sort=publishing_date+desc%2Ctitle_sort+asc&rows=1')).json()
+	const response = await got.get(new URL('https://suche.transparenz.hamburg.de/api/3/action/package_search?q=name%3Ahvv-fahrplandaten-gtfs-%2A-bis%2A&sort=metadata_modified%20desc')).json()
 	const item = response?.result?.results[0]
 	if (!item || item.author !== 'Hamburger Verkehrsverbund GmbH') throw new Error('no matching dataset found')
 	if (item.license_id !== 'dl-de-by-2.0') throw new Error('unexpected license')
@@ -13,6 +13,8 @@ export const hvvGtfs = async () => {
 	// throw if latest file is older than 31 days
 	if (+new Date() - (+new Date(created)) > 31 * 24 * 60 * 60 * 1000) throw new Error(`latest dataset seems to be outdated: ${created}`)
 
-	const stream = await got.stream.get(url)
+	const stream = await got.stream.get(url, {
+		https: { rejectUnauthorized: false }, // sigh…
+	})
 	return stream.pipe(process.stdout)
 }
